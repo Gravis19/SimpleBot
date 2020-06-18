9366718d21ec037fcb411cab92e9b648df18462c
Gravis19 <66223742+Gravis19@users.noreply.github.com>
2020-06-18 17:01:26 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,104 +1,158 @@
-package tictactoe;
-
-
+package readability;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
 import java.util.Scanner;
-
 public class Main {
+    static int countOfWords;
+    static int countOfSentences;
+    static int countOfChar;
+    static int countOfSyllables;
+    static int countOfPolysyllables;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        char[] cell = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-        boolean notFinished = true;
-        char ch = 'O';
-        int x = 0;
-        int o = 0;
-        gameField(cell, ' ', 0);
-        while (notFinished) {
-            if (wins(cell)) {
-                System.out.println(ch + " wins");
-                break;
+
+        String text = "";
+        try (Scanner scanner = new Scanner(new File(args[0]))) {
+            while (scanner.hasNext()) {
+                text = text.concat(scanner.nextLine().concat(" "));
             }
-            System.out.println("Enter the coordinates: ");
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            ch = ch == 'O' ? 'X' : 'O';
-            if(notFinished(cell)) {
-                if (a < 4 && a > 0 && b < 4 && b > 0) {
-                    if (occupied(cell, coordinates(a, b))){
-                        System.out.println("This cell is occupied! Choose another one!");
-                    }else {
-                        gameField(cell, ch, coordinates(a, b));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        String[] sentences = text.trim().split("[\\.\\!\\?]");
+        countOfSentences = sentences.length;
+        countOfChar = text.replaceAll("\\s+", "").length();
+        for (String sentence : sentences) {
+            String[] words = sentence.trim().split("\\s+");
+            countOfWords += words.length;
+            for (String word : words) {
+                word = word.toLowerCase().trim();
+                if (word.matches(".+e")) {
+                    word = word.substring(0, word.length() - 1);
+                }
+                if (word.length() < 3) {
+                    countOfSyllables++;
+                } else {
+                    int syllables = word.split("[aeiouy]+", -1).length - 1;
+                    countOfSyllables += syllables;
+                    if (syllables > 2) {
+                        countOfPolysyllables++;
                     }
                 }
-                else {
-                    System.out.println("Coordinate should be from 1 to 3!");
-                }
-            } else {
-                notFinished = notFinished(cell);
-                System.out.println("Draw");
             }
         }
+        System.out.println("Words: " + countOfWords);
+        System.out.println("Sentences: " + countOfSentences);
+        System.out.println("Characters: " + countOfChar);
+        System.out.println("Syllables: " + countOfSyllables);
+        System.out.println("Polysyllables: " + countOfPolysyllables);
+        System.out.println("Enter the score you want to calculate (ARI, FK, SMOG, CL, all):");
+        Scanner scanner = new Scanner(System.in);
+        countTheScore(scanner.next());
+
     }
-    public static int coordinates(int a, int b) {
-        {
-            if (a == 1) {
-                return b == 1 ? 6 : b == 2 ? 3 : 0;
-            }
-            if (a == 2) {
-                return b == 1 ? 7 : b == 2 ? 4 : 1;
-            }
-            else {
-                return b == 1 ? 8 : b == 2 ? 5 : 2;
-            }
+    static int getAgeByScore(double score) {
+        score = Math.round(score);
+        int age = 0;
+        switch ((int) score) {
+            case 1:
+                System.out.println("(about 6 year olds).");
+                age = 6;
+                break;
+            case 2:
+                System.out.println("(about 7 year olds).");
+                age = 7;
+                break;
+            case 3:
+                System.out.println("(about 9 year olds).");
+                age = 9;
+                break;
+            case 4:
+                System.out.println("(about 10 year olds).");
+                age = 10;
+                break;
+            case 5:
+                System.out.println("(about 11 year olds).");
+                age = 11;
+                break;
+            case 6:
+                age = 12;
+                System.out.println("(about 12 year olds).");
+                break;
+            case 7:
+                System.out.println("(about 13 year olds).");
+                age = 13;
+                break;
+            case 8:
+                System.out.println("(about 14 year olds).");
+                age = 14;
+                break;
+            case 9:
+                System.out.println("(about 15 year olds).");
+                age = 15;
+                break;
+            case 10:
+                System.out.println("(about 16 year olds).");
+                age = 16;
+                break;
+            case 11:
+                System.out.println("(about 17 year olds).");
+                age = 17;
+                break;
+            case 12:
+                System.out.println("(about 18 year olds).");
+                age = 18;
+                break;
+            case 13:
+                System.out.println("(about 24 year olds).");
+                age = 24;
+                break;
+            case 14:
+                System.out.println("(about 25+ year olds).");
+                age = 25;
+                break;
         }
+        return age;
     }
-    public static char[] gameField(char[] cell, char ch, int coordinate) {
-        cell[coordinate] = ch;
-        System.out.println("---------------------------");
-        System.out.println("| " + cell[0] + " " + cell[1] + " " + cell[2] + " |");
-        System.out.println("| " + cell[3] + " " + cell[4] + " " + cell[5] + " |");
-        System.out.println("| " + cell[6] + " " + cell[7] + " " + cell[8] + " |");
-        System.out.println("---------------------------");
-        return cell;
-    }
-    public static boolean wins(char[] cell) {
-        boolean wins = false;
-        for (int i = 0; i < 7; i += 3) {
-            if (cell[i] != '_' && cell[i] != ' ') {
-                if (cell[i] == cell[i + 1] && cell[i] == cell[i + 2]) {
-                    wins = true;
-                    break;
-                }
-                else wins =  false;
-            }
-        } if (!wins) {
-            for (int i = 0; i < 3; i++) {
-                if (cell[i] != '_' && cell[i] != ' ') {
-                    if (cell[i] == cell[i + 3] && cell[i + 3] == cell[i + 6]) {
-                        wins = true;
-                        break;
-                    } else wins = false;
-                }
-            }
+    static void countTheScore(String rIndex) {
+        double scoreARI = 4.71 * countOfChar / countOfWords + 0.5 * countOfWords / countOfSentences - 21.43;
+        double scoreFK = 0.39 * countOfWords / countOfSentences + 11.8 * countOfSyllables / countOfWords - 15.59;
+        double scoreSMOG = 1.043 * Math.sqrt(countOfPolysyllables * 30 / countOfSentences) + 3.1291;
+        double scoreCL = 0.0588 * countOfChar / countOfWords * 100 - 0.296 * countOfSentences / countOfWords * 100 - 15.8;
+        int ageARI = 0;
+        int ageFK = 0;
+        int ageSMOG = 0;
+        int ageCL = 0;
+        switch (rIndex.toLowerCase()) {
+            case "ari":
+                System.out.printf ("Automated Readability Index: %.2f ", scoreARI);
+                getAgeByScore(scoreARI);
+                break;
+            case "fk":
+                System.out.printf("Flesch–Kincaid readability tests: %.2f ", scoreFK);
+                getAgeByScore(scoreFK);
+                break;
+            case "smog":
+                System.out.printf("Simple Measure of Gobbledygook: %.2f ", scoreSMOG);
+                getAgeByScore(scoreSMOG);
+                break;
+            case "cl":
+                System.out.printf("Coleman–Liau index: %.2f ",scoreCL);
+                getAgeByScore(scoreCL);
+                break;
+            case "all":
+                System.out.printf ("Automated Readability Index: %.2f ", scoreARI);
+                ageARI = getAgeByScore(scoreARI);
+                System.out.printf("Flesch–Kincaid readability tests: %.2f", scoreFK);
+                ageFK = getAgeByScore(scoreFK);
+                System.out.printf("Simple Measure of Gobbledygook: %.2f ", scoreSMOG);
+                ageSMOG = getAgeByScore(scoreSMOG);
+                System.out.printf("Coleman–Liau index: %.2f ",scoreCL);
+                ageCL = getAgeByScore(scoreCL);
+                double averageAge = (ageARI + ageFK + ageSMOG + ageCL) / 4.0;
+                System.out.printf("This text should be understood in average by %.2f year olds.\n", averageAge);
+                break;
         }
-        if (cell[4] != '_' && cell[4] != ' ' && !wins) {
-            if ((cell[0] == cell[4] && cell[0] == cell[8]) || (cell[2] == cell[4] && cell[2] == cell[6])) {
-                wins = true;
-            }
-            else wins =  false;
-        }
-        return wins;
-    }
-    public static boolean notFinished(char[] cell) {
-        for (int i = 0; i < 9; i++) {
-            if (cell[i] == '_' || cell[i] == ' ') {
-                return true;
-            }
-        } return false;
-    }
-    public static boolean occupied (char[] cell, int coordinate) {
-        if (cell[coordinate] == 'X' || cell[coordinate] == 'O') {
-            return true;
-        }
-        else return false;
+
     }
 }
